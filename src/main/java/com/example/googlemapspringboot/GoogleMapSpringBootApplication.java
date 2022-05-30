33fa33100cba41fd5fe830ef81245961f25009ca
Sedat Doğru <46866955+Sdtdogru@users.noreply.github.com>
2022-05-30 13:05:53 +0300

    create spring boot project

---
+++ src/main/java/com/example/googlemapspringboot/GoogleMapSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.googlemapspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleMapSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleMapSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/googlemapspringboot/GoogleMapSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.googlemapspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleMapSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
